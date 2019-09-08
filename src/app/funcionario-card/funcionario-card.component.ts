a458cc8ab3040b56d1093c3866f48f143504b6b7
erikflima <erik.alves253@gmail.com>
2019-09-07 23:56:35 -0300

    Curso: Fullstack Angular e Spring - Julho-2018
     Video: 9.17. Estilos CSS dinâmicos com ngStyle
    
     Usando o comando "ngStyle" para fazer css dinamico

--- src/app/funcionario-card/funcionario-card.component.ts
+++ src/app/funcionario-card/funcionario-card.component.ts
@@ -22,6 +22,19 @@ export class FuncionarioCardComponent{
    //Criando variaveis globais
 
    /*A instrucao "@Input()" faz com que essa variavel seja capaz de receber um valor vindo de outro componente. Nesse exemplo, o valor vem do arquivo "app.component.html" */
-   @Input() funcionarioDoCard: any; 
+   @Input() funcionarioDoCard: any;
+
+   numeroExemplo = 1;
+
+
+   //Metodo para propriedades css
+   getEstilosCartao(){    
+
+    /*Colocando uma condicao para retornar a propriedade css que desejo.
+      Se o numero exemplo for 'par', entao retorna 'lightblue', se nao retorna 'lightgreen' */
+     return {backgroundColor: this.numeroExemplo % 2 === 0   ?   'lightblue' : 'lightgreen'};
+
+
+   }
 
 }
