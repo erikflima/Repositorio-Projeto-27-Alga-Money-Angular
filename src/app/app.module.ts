830a8743a2bff2802df3743e065c59d1a8062ec0
erikflima <erik.alves253@gmail.com>
2019-09-16 21:46:00 -0300

    Curso: Fullstack Angular e Spring - Julho-2018
    Video: 10.5. Adicionando uma tabela de dados
    
    - Usando o componente <p-dataTable> do primeNg 4
      Referencia -> https://www.primefaces.org/primeng-4.3.0/#/datatable

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule }      from '@angular/core';
-import { AppComponent }  from './app.component';
-import {InputTextModule} from 'primeng/primeng'; //Import para poder usar a tag '<input pInputText type="text">' da biblioteca do primeng 4 no arquivo 'app.component.html'.
-import {ButtonModule}    from 'primeng/primeng'; //Import para poder usar a tag '<button pButton>' da biblioteca do primeng 4 no arquivo 'app.component.html'.
-
+import { BrowserModule }              from '@angular/platform-browser';
+import { NgModule }                   from '@angular/core';
+import { AppComponent }               from './app.component';
+import {InputTextModule}              from 'primeng/primeng'; //Import para poder usar a tag '<input pInputText type="text">' da biblioteca do primeng 4 no arquivo 'app.component.html'.
+import {ButtonModule}                 from 'primeng/primeng'; //Import para poder usar a tag '<button pButton>' da biblioteca do primeng 4 no arquivo 'app.component.html'.
+import {DataTableModule}              from 'primeng/primeng'; //Import para poder usar a tag '<p-dataTable>' da biblioteca do primeng 4 no arquivo 'app.component.html'.
 
 @NgModule({
 
@@ -14,7 +14,8 @@ import {ButtonModule}    from 'primeng/primeng'; //Import para poder usar a tag 
   imports: [
     BrowserModule,
     InputTextModule,
-    ButtonModule
+    ButtonModule,
+    DataTableModule
   ],
 
   providers: [],
