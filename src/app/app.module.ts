c89fb2fb0b8a143212797a6b03da747339290f94
erikflima <erik.alves253@gmail.com>
2019-09-19 23:04:16 -0300

    Video: 10.10. Criando o componente de pesquisa de lançamentos
    Curso: Fullstack Angular e Spring - Julho-2018
    
    Criando o componente "lancamentos-pesquisa"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule }      from '@angular/core';
-import { AppComponent }  from './app.component';
-import {InputTextModule} from 'primeng/primeng'; //Import para poder usar a tag '<input pInputText type="text">' da biblioteca do primeng 4 no arquivo 'app.component.html'.
-import {ButtonModule}    from 'primeng/primeng'; //Import para poder usar a tag '<button pButton>' da biblioteca do primeng 4 no arquivo 'app.component.html'.
-import {DataTableModule} from 'primeng/primeng'; //Import para poder usar a tag '<p-dataTable>' da biblioteca do primeng 4 no arquivo 'app.component.html'.
-import {TooltipModule}   from 'primeng/primeng'; //Import para poder usar a instrucao 'pTooltip' da biblioteca do primeng 4 no arquivo 'app.component.html'.
+import { BrowserModule }                from '@angular/platform-browser';
+import { NgModule }                     from '@angular/core';
+import { AppComponent }                 from './app.component';
+import {InputTextModule}                from 'primeng/primeng'; //Import para poder usar a tag '<input pInputText type="text">' da biblioteca do primeng 4 no arquivo 'app.component.html'.
+import {ButtonModule}                   from 'primeng/primeng'; //Import para poder usar a tag '<button pButton>' da biblioteca do primeng 4 no arquivo 'app.component.html'.
+import {DataTableModule}                from 'primeng/primeng'; //Import para poder usar a tag '<p-dataTable>' da biblioteca do primeng 4 no arquivo 'app.component.html'.
+import {TooltipModule}                  from 'primeng/primeng'; //Import para poder usar a instrucao 'pTooltip' da biblioteca do primeng 4 no arquivo 'app.component.html'.
+import { LancamentosPesquisaComponent } from './lancamentos-pesquisa/lancamentos-pesquisa.component';
 
 
 @NgModule({
 
   declarations: [
-    AppComponent
+    AppComponent,
+    LancamentosPesquisaComponent
   ],
 
   imports: [
