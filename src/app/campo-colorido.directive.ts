3668204bd2f3f1e34d0c2fb9aa2627f7d72c3cd6
erikflima <erik.alves253@gmail.com>
2019-09-25 22:59:16 -0300

    Video: 11.4. Usando property binding em diretivas customizadas
    Curso: Fullstack Angular e Spring - Julho-2018
    
    Diretivas: São instruções do angular que servem para alterar comportamento ou aparencia de algum  elemento.
    
     - Trocando a cor de fundo do elemento que estou referenciando
    
     - Usando a anotação "@Input()"

--- src/app/campo-colorido.directive.ts
+++ src/app/campo-colorido.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, HostListener, HostBinding } from '@angular/core';
+import { Directive, HostListener, HostBinding, Input } from '@angular/core';
 
 //Diretiva que criei. 'Diretivas: São instruções do angular que servem para alterar comportamento ou aparencia de algum  elemento.
 @Directive({
@@ -6,17 +6,19 @@ import { Directive, HostListener, HostBinding } from '@angular/core';
 })
 export class CampoColoridoDirective {
 
+  //
+  @Input() cor = 'gray';
+
   //@HostBinding -> Serve para que eu possa ter acesso a propriedade do elemento que possui a diretiva que criei (no caso a diretiva que criei foi'appCampoColorido')
   @HostBinding('style.backgroundColor') corDeFundo: string;
-
 
 
   /*Esse metodo sera executado quando o campo "<input>" da pagina "funcionario-form.component.html" receber focus.
     A anotacao "@HostListener" serve justamente para dizer qual o evento, como por exemplo "focus", "blur", "click", "mouseleave", entre outros. */
   @HostListener('focus') aoGanharFoco() {
 
-    console.log( "Trocando a cor de fundo do elemento que estou referenciando para amarelo");
-    this.corDeFundo = 'yellow';
+    console.log( "Trocando a cor de fundo do elemento que estou referenciando");
+    this.corDeFundo = this.cor;
   }
 
 
