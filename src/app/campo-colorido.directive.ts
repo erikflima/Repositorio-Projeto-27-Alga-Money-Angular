48a10a5d23889a03abd598ec4c12ed8acb583e7c
erikflima <erik.alves253@gmail.com>
2019-09-25 22:31:27 -0300

    Video: 11.2. Respondendo a eventos do hospedeiro com @HostListener
    Curso: Fullstack Angular e Spring - Julho-2018
    
    Diretivas: São instruções do angular que servem para alterar comportamento ou aparencia de algum  elemento.
    
     - Trocando a cor de fundo do elemento que estou referenciando
    
     - Usando a anotação "@HostBinding"

--- src/app/campo-colorido.directive.ts
+++ src/app/campo-colorido.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, Renderer2, HostListener } from '@angular/core';
+import { Directive, HostListener, HostBinding } from '@angular/core';
 
 //Diretiva que criei. 'Diretivas: São instruções do angular que servem para alterar comportamento ou aparencia de algum  elemento.
 @Directive({
@@ -6,25 +6,17 @@ import { Directive, ElementRef, Renderer2, HostListener } from '@angular/core';
 })
 export class CampoColoridoDirective {
 
-
-  //ElementRef -> Eh um objeto que o angular injeta pra mim, e posso usar esse objeto para acessar o "elemento hospedero", ou seja, um elemento especifico que esteja na pagina que eu estiver mexendo.
-  //Renderer2  -> Eh um objeto que o angular injta pra mim, que posso usar para manipular elementos
-  constructor( private referenciadorDeElemento: ElementRef, private renderer: Renderer2 ){
-
-    console.log( "Elemento html que estou referenciando e que posso manipular: ");
-    console.log( this.referenciadorDeElemento.nativeElement );
-    console.log( "\n");
+  //@HostBinding -> Serve para que eu possa ter acesso a propriedade do elemento que possui a diretiva que criei (no caso a diretiva que criei foi'appCampoColorido')
+  @HostBinding('style.backgroundColor') corDeFundo: string;
 
 
-  }
 
   /*Esse metodo sera executado quando o campo "<input>" da pagina "funcionario-form.component.html" receber focus.
     A anotacao "@HostListener" serve justamente para dizer qual o evento, como por exemplo "focus", "blur", "click", "mouseleave", entre outros. */
   @HostListener('focus') aoGanharFoco() {
 
     console.log( "Trocando a cor de fundo do elemento que estou referenciando para amarelo");
-    this.renderer.setStyle( this.referenciadorDeElemento.nativeElement, 'background-color', 'yellow' );
-
+    this.corDeFundo = 'yellow';
   }
 
 
@@ -33,7 +25,7 @@ export class CampoColoridoDirective {
   @HostListener('blur') aoPerderFoco() {
 
     console.log( "Trocando a cor de fundo do elemento que estou referenciando para transparent");
-    this.renderer.setStyle( this.referenciadorDeElemento.nativeElement, 'background-color', 'transparent' );
+    this.corDeFundo = 'transparent';
   }
 
 
